7e6e91938d983063892eea2f5d557e7b82dbe70b
GoreC1 <hazich322@gmail.com>
2022-12-01 18:15:43 +0500

    UnitOfWork

--- RegistryOfEstablisment/Controller/EnterpriseController.cs
+++ RegistryOfEstablisment/Controller/EnterpriseController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RegistryOfEstablisment.Model.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace RegistryOfEstablisment.Controller
 {
     class EnterpriseController
     {
+
+        public Enterprise GetEnterprise(int enterpriseID)
+        {
+            return new Enterprise();
+        }
     }
 }

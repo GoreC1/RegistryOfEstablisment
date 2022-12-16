597171a035d00158c916c805f135f81925ce3e1d
GoreC1 <hazich322@gmail.com>
2022-12-16 23:47:50 +0500

    Исправления в фильтре и заполнении полей

--- RegistryOfEstablisment/View/AuthorisationForm.Designer.cs
+++ RegistryOfEstablisment/View/AuthorisationForm.Designer.cs
@@ -111,6 +111,7 @@ namespace RegistryOfEstablisment
             this.Name = "AuthorisationForm";
             this.Text = "Авторизация";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.AuthorisationForm_FormClosing);
+            this.Load += new System.EventHandler(this.AuthorisationForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
